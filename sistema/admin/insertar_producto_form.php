7520b4852a66c848171a0cf6d6915eebfb009a82
elindustries <extremixtlocust@gmail.com>
2021-07-01 13:12:37 -0500

    selección de foto

--- sistema/admin/insertar_producto_form.php
+++ sistema/admin/insertar_producto_form.php
@@ -14,6 +14,7 @@ $resultado_categorias = mysqli_query($conn, $sql_categorias);
         <meta charset="utf-8">
         <!-- Bootstrap CSS -->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+            <link rel="stylesheet" href="../../css/subirFoto.css" class="hre">
             
         <meta charset="utf-8">
         <title>Insertar Producto</title>
@@ -24,7 +25,7 @@ $resultado_categorias = mysqli_query($conn, $sql_categorias);
             <h1>Abarrotes Zavala</h1>
             <h2>Ingrese los datos del producto</h2>
             <div class="container">
-                <form id="form1" method="POST" action="insertar_producto_exe.php" class="needs-validation">
+                <form method="POST" action="insertar_producto_exe.php" enctype="multipart/form-data" class="needs-validation">
                     <!-- Proveedores -->
                     <label>Proveedor</label>
                     <div class="input-group mb-3">
@@ -74,15 +75,31 @@ $resultado_categorias = mysqli_query($conn, $sql_categorias);
                             <a href="insertar_categoria_form.php" class="btn btn-outline-secondary" type="button">Insertar categoría</a>
                         </div>
                     </div>
+                    <!-- Foto del producto -->
+                    <div class="mb-3">
+                        <div class="photo">
+                            <label for="foto">Foto</label>
+                                <div class="prevPhoto">
+                                <span class="delPhoto notBlock">X</span>
+                                <label for="foto"></label>
+                                </div>
+                                <div class="upimg">
+                                <input type="file" name="foto" id="foto">
+                                </div>
+                                <div id="form_alert"></div>
+                        </div>
+                    </div>
+                    
 
                     <!-- Botón de registro -->
                     <div class="mb-3">
-                        <button type="submit" class="btn btn-primary" id="btn">Enviar</button>
+                        <button type="submit" class="btn btn-primary" id="btn">Guardar producto</button>
                     </div>
                     <!-- Botón de regreso -->
                     <div class="mb-3">
                         <a class="btn btn-secondary" href="tabla_productos.php" role="button">Atrás</a>
                     </div>
+
                 </form>
 
             </div>
@@ -99,5 +116,6 @@ $resultado_categorias = mysqli_query($conn, $sql_categorias);
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="../../js/jquery-3.6.0.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="../../js/subirFoto.js"></script>
     </body>
 </html>
